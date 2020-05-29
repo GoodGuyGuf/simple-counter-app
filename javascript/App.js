416b389c5d6c50ab61381f9005d56123ac647b24
Ethan <ethan_gustafson@outlook.com>
2020-05-28 22:45:03 -0400

    added index.js/App.js + app logo renders on page

---
+++ javascript/App.js
@@ -0,0 +1,12 @@
+class App{
+    constructor(){
+        this.div = document.createElement("div");
+        this.div.id = "logoContainer";
+        document.body.appendChild(this.div);
+        
+        this.logo = document.createElement("h1");
+        this.logo.id = "logo";
+        this.logo.innerHTML = "Counter";
+        this.div.appendChild(this.logo)
+    }
+}
