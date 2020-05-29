d14fe0fb3c5310898d1ca9919b218a9923df5302
Ethan <ethan_gustafson@outlook.com>
2020-05-28 23:15:19 -0400

    added forms for both initial & ending input for countdown

--- javascript/countdown/Countdown.js
+++ javascript/countdown/Countdown.js
@@ -9,23 +9,31 @@ class Countdown{
         this.logo.innerHTML = "Countdown";
         this.div.appendChild(this.logo);
 
-        this.initialNumberContainer = document.createElement("div");
+        this.initialNumberContainer = document.createElement("div"); // initial number div
         this.initialNumberContainer.id = "initialNumberContainer";
         document.body.appendChild(this.initialNumberContainer);
 
-        this.initialNumber = document.createElement("h3");
+        this.initialNumber = document.createElement("h3"); // initial number h3
         this.initialNumber.id = "initialNumber";
         this.initialNumber.innerHTML = "Input a number value to count down from:";
         this.initialNumberContainer.appendChild(this.initialNumber);
 
-        this.endingNumberContainer = document.createElement("div");
+        this.initialNumberForm = document.createElement("form"); // initial number form
+        this.initialNumberForm.innerHTML = `<input type="text" name="initialNumber"></input><input type="submit" value="Submit"></input>`;
+        this.initialNumberContainer.appendChild(this.initialNumberForm);
+
+        this.endingNumberContainer = document.createElement("div"); // ending number div
         this.endingNumberContainer.id = "endingNumberContainer";
         document.body.appendChild(this.endingNumberContainer);
 
-        this.endingNumber = document.createElement("h3");
+        this.endingNumber = document.createElement("h3"); // ending number h3
         this.endingNumber.id = "endingNumber";
         this.endingNumber.innerHTML = "Input a number value where the timer will stop:";
         this.endingNumberContainer.appendChild(this.endingNumber);
+
+        this.endingNumberForm = document.createElement("form"); // ending number form
+        this.endingNumberForm.innerHTML = `<input type="text" name="endingNumber"></input><input type="submit" value="Submit"></input>`;
+        this.endingNumberContainer.appendChild(this.endingNumberForm);
         
     }
 }
