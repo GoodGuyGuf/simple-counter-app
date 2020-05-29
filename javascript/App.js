dd469fac1f41367d9a76b9d22fd204a1be976002
Ethan <ethan_gustafson@outlook.com>
2020-05-28 22:57:27 -0400

    when increment/decrement buttons are clicked, DOM updates counter

--- javascript/App.js
+++ javascript/App.js
@@ -26,5 +26,27 @@ class App{
         this.counterIncrement.id = "increment";
         this.counterIncrement.innerHTML = "+";
         this.counterIncrementContainer.appendChild(this.counterIncrement);
+
+        this.counter = document.createElement("h2");
+        this.counter.id = "counter";
+        this.counter.innerHTML = 0;
+        document.body.appendChild(this.counter);
+
+        this.handleIncrement();
+        this.handleDecrement();
+    }
+
+    handleIncrement = () => {
+        this.counterIncrement.addEventListener("click", () => {
+            const increment = parseInt(this.counter.innerHTML) + 1;
+            this.counter.innerHTML = increment;
+        })
+    }
+
+    handleDecrement = () => {
+        this.counterDecrement.addEventListener("click", () => {
+            const decrement = parseInt(this.counter.innerHTML) - 1;
+            this.counter.innerHTML = decrement;
+        })
     }
 }
