af2ff1406d64d2f305e0038db2fd8b7e5734d375
Ethan <ethan_gustafson@outlook.com>
2020-05-28 23:04:29 -0400

    added Countdown class with logo rendered to DOM

--- javascript/countdown/Countdown.js
+++ javascript/countdown/Countdown.js
@@ -1,16 +1,12 @@
-class App{
+class Countdown{
     constructor(){
         this.div = document.createElement("div");
-        this.div.id = "logoContainer";
+        this.div.id = "countdownContainer";
         document.body.appendChild(this.div);
 
         this.logo = document.createElement("h1");
-        this.logo.id = "counterLogo";
-        this.logo.innerHTML = "Counter";
+        this.logo.id = "countdownLogo";
+        this.logo.innerHTML = "Countdown";
         this.div.appendChild(this.logo)
-
-        new Counter;
-        new Countdown;
     }
-
 }
