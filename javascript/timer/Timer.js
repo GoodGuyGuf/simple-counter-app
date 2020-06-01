4c3cde40fff8705d7218c7037aff25f19262c920
Ethan <ethan_gustafson@outlook.com>
2020-06-01 05:22:19 -0400

    added timer start, pause and reset buttons

--- javascript/timer/Timer.js
+++ javascript/timer/Timer.js
@@ -4,5 +4,25 @@ class Timer{
         this.div.id = "TimerContainer";
         this.div.innerHTML = `<h1>Timer</h1>`;
         document.body.appendChild(this.div);
+
+        this.time = document.createElement("h2");
+        this.time.id = "time";
+        this.time.innerHTML = 0;
+        this.div.appendChild(this.time);
+
+        this.startButton = document.createElement("button");
+        this.startButton.id = "timerStart";
+        this.startButton.innerHTML = "Start";
+        this.div.appendChild(this.startButton);
+
+        this.pauseButton = document.createElement("button");
+        this.pauseButton.id = "timerPause";
+        this.pauseButton.innerHTML = "Pause";
+        this.div.appendChild(this.pauseButton);
+
+        this.resetButton = document.createElement("button");
+        this.resetButton.id = "timerReset";
+        this.resetButton.innerHTML = "Reset";
+        this.div.appendChild(this.resetButton);
     }
 }
